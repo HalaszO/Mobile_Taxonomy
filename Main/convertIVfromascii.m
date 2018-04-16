17d7764932bcf233f0b112a029b90c5fd3916084
Olivér Halász <31652741+HalaszO@users.noreply.github.com>
2018-04-16 18:04:20 +0200

    Add files via upload
    Changed values to fit the new protocol

--- Main/convertIVfromascii.m
+++ Main/convertIVfromascii.m
@@ -1,4 +1,4 @@
-clear all
+%clear all
 close all
 %%%%%%%%%%%%
 % Don't forget to add folders to path!!!
@@ -22,14 +22,19 @@ addpath('C:\Users\Oliver\Desktop\Egyetem\7\Szakdoga');
 % asciipath = 'C:\Users\Oliver\Desktop\Egyetem\7\Szakdoga\HBP_data_all_grouped_szakdoga\PV\STEP HBP';
 % savepath='C:\Users\Oliver\Desktop\Egyetem\7\Szakdoga\tmp\IV\OTHER\STEP_HBP';
 % sourcepath='C:\Users\Oliver\Desktop\Egyetem\7\Szakdoga\tmp';
-asciipath = 'C:\Users\Oliver\Desktop\Egyetem\7\Szakdoga\Elphys_all_fixed\Other\STEP_LONG';
-savepath='C:\Users\Oliver\Desktop\Egyetem\7\Szakdoga\Elphys_all_fixed_data_again\IV\Other'; %Important to add a subfolder in IV folder
-sourcepath='C:\Users\Oliver\Desktop\Egyetem\7\Szakdoga\Elphys_all_fixed_data_again';
+%asciipath = 'C:\Users\Oliver\Desktop\Egyetem\7\Szakdoga\Elphys_all_fixed\Other\STEP_LONG';
+%savepath='C:\Users\Oliver\Desktop\Egyetem\7\Szakdoga\Elphys_all_fixed_data_again\IV\Other'; %Important to add a subfolder in IV folder
+%sourcepath='C:\Users\Oliver\Desktop\Egyetem\7\Szakdoga\Elphys_all_fixed_data_again';
 
+asciipath = 'C:\Users\Oliver\Desktop\Egyetem\7\Szakdoga\Makara_Judit_rat\Makara_Judit_rat\CA1';
+savepath='C:\Users\Oliver\Desktop\Egyetem\7\Szakdoga\Makara\IV\CA1'; %Important to add a subfolder in IV folder
+sourcepath='C:\Users\Oliver\Desktop\Egyetem\7\Szakdoga\Makara';
 % STEP LONG:
-current=[+10,-10,20,-20,30,-30,40,-40,50,-50,60,-60,70,-70,80,-80,90,-90,100,-100,150,200,250,300,400,500,600];
+%current=[+10,-10,20,-20,30,-30,40,-40,50,-50,60,-60,70,-70,80,-80,90,-90,100,-100,150,200,250,300,400,500,600];
 %STEP HBP:
 %current=[+20,-20,40,-40,60,-60,80,-80,100,-100,120,-120,140,-140,160,-160,180,-180,200,220,240,260,280,300,400,500,600];
+%
+current = [0.05, 0.1, 0.15, 0.2, 0.25, -0.05, -0.1, -0.15, -0.2, -0.25];
 sampleinterval = 0.0002;	
 
 Folder_Checker_Taxonomy(sourcepath,savepath);
@@ -51,7 +56,7 @@ for i=1:length(fnames)
 %     else
 %         current=stepcurrent;
 %     end
-    temp=strfind(fname,'_cols.txt');
+    temp=strfind(fname,'.txt');
     if isempty(temp)
         temp=strfind(fname,'_columnwise.txt');
     end
@@ -59,7 +64,7 @@ for i=1:length(fnames)
     fname=fname(1:temp-1);
     
     
-    temp=strfind(fname,'-');
+    temp=strfind(fname,'c');
     cellname=['cell_',fname(temp+1:end)];
     
     fname=fname(1:temp-1);
@@ -111,7 +116,7 @@ for i=1:length(fnames)
     iv.(cellname).sweepnum=ii;
     iv.(cellname).time=[0:sampleinterval:sampleinterval*(size(temp,1)-1)];
     iv.(cellname).time=iv.(cellname).time';
-    iv.(cellname).segment=[200,800,400];
+    iv.(cellname).segment=[100,300,400];
 end
 spath = [savepath filesep lastfname];
 save(spath, 'iv');
@@ -120,3 +125,4 @@ clear iv
 %sourcepath='/home/halasz/mobile_taxonomy/MATLABdata_Katona';
 %sourcepath='/home/heivi/mobile_taxonomy/MATLABdata_zsolt_new';
 HEKA_getgoodIVs_main(sourcepath);
+% 
